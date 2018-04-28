e2854f9e15ce4c8ad5b5c81ecbbadec747ae48fe
funkb <texasfunk101@gmail.com>
2018-04-28 12:26:18 -0500

    Update Digits.java

--- Digits/Digits.java
+++ Digits/Digits.java
@@ -6,11 +6,17 @@ public class Digits {
 	public Digits(int num) //Given
 	{
 		digitList = new ArrayList<Integer>(); //Initialize the ArrayList
+/*  STRING METHOD
 		String num1 = String.valueOf(num);    //Convert num to a string
 		for (int i = 0; i < num1.length(); i++) {
 			digitList.add(Integer.parseInt(num1.substring(i, i+1)));   //Add every digit to the ArrayList
 		}
-		
+*/
+//Number Method
+    while (num>0){
+        digitList.add(0,(num - ((int)(num/10)*10));// ArrayList.add (index, value)
+        num = (int)(num/10);//Delete value at the end of our number.
+    }
 		System.out.println(isStrictlyIncreasing()); //Print out the result of our method.
 	}
 	
