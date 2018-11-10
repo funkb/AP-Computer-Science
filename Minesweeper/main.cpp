695a485547576e1705d5b452f454c4f23678ee62
funkb <texasfunk101@gmail.com>
2018-11-10 17:45:27 -0600

    Add files via upload

--- Minesweeper/main.cpp
+++ Minesweeper/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 
 using namespace std;
 
-const int GRID_SIZE = 10;
-const int MINES = 20;
+const int GRID_SIZE = 5;
+const int MINES = 2;
 bool running = true;
 
 void print_menu(int& input);
@@ -16,8 +17,10 @@ void print_array(char (&arr)[GRID_SIZE][GRID_SIZE]);
 void generate_adjacent(char (&arr)[GRID_SIZE][GRID_SIZE]);
 int get_surr(char (&arr)[GRID_SIZE][GRID_SIZE], int x, int y);
 void reveal_square(char (&arr_vis)[GRID_SIZE][GRID_SIZE], char (&arr_guide)[GRID_SIZE][GRID_SIZE]);
-void place_flag();
+bool place_flag(char (&arr_vis)[GRID_SIZE][GRID_SIZE], char(&arr_guide)[GRID_SIZE][GRID_SIZE]);
 bool in_grid(int x, int y);
+void get_path(char (&arr_guide)[GRID_SIZE][GRID_SIZE], char(&arr_vis)[GRID_SIZE][GRID_SIZE], int x, int y);
+void remove_flag(char (&arr_vis)[GRID_SIZE][GRID_SIZE]);
 
 int main(){
 	srand(time(NULL));
@@ -31,7 +34,7 @@ int main(){
 	populate(map_guide);
 	
 	while (running){
-		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+		system("CLS");
 		int option=0;
 		print_array(map_visable);
 		print_menu(option);
@@ -44,9 +47,12 @@ int main(){
 					reveal_square(map_visable, map_guide);
 					break;
 				case 2:
-					place_flag();
+					if(place_flag(map_visable,map_guide)) return 0;
 					break;
 				case 3:
+					remove_flag(map_visable);
+					break;
+				case 4:
 					cout << "Thanks for playing!" << endl; 
 					running=false;
 					break;
@@ -66,7 +72,7 @@ int main(){
 
 void print_menu(int& input){
 	cout << "====================" << endl;
-	cout << "1. Reveal Square\n2. Place Flag\n3. Quit" << endl;
+	cout << "1. Reveal Square\n2. Place Flag\n3. Remove Flag\n4. Quit" << endl;
 	cout << "====================" << endl;
 	cout << ">";
 	cin >> input;
@@ -81,7 +87,7 @@ void fill_map(char (&arr)[GRID_SIZE][GRID_SIZE], char fill){
 }
 
 void populate(char (&arr)[GRID_SIZE][GRID_SIZE]){
-	int count = 20;
+	int count = MINES;
 	while (count > 0){
 		place_mine(arr);
 		count--;
@@ -113,6 +119,9 @@ void generate_adjacent(char (&arr)[GRID_SIZE][GRID_SIZE]){
 		for (int j = 0; j < GRID_SIZE; j++){
 			if(!(arr[i][j] == '@'))
 				arr[i][j] = '0' + get_surr(arr, i,j);
+				if(arr[i][j] == '0') {
+					arr[i][j] = '.';
+				}
 		}
 	}
 }
@@ -136,20 +145,82 @@ bool in_grid(int x, int y){
 void reveal_square(char (&arr_vis)[GRID_SIZE][GRID_SIZE], char (&arr_guide)[GRID_SIZE][GRID_SIZE]){
 	int x,y;
 	cout << "Which square would you like to reveal? (1-10) (x y)> ";
-	cin >> x >> y;
+	cin >> y >> x;
 	x-=1;
 	y-=1;
 	if(in_grid(x,y)){
 		arr_vis[x][y] = arr_guide[x][y];
+		if(arr_vis[x][y] == '.'){
+			get_path(arr_guide, arr_vis, x,y);
+		}
 		if(arr_vis[x][y] == '@'){
 			print_array(arr_vis);
 			cout << "\nYou Lose!" << endl;
 			running=false;
 		}
+	}else{
+		cout << "That point does not exist!" << endl;
 	}
 }
 
-void place_flag(){
+void get_path(char (&arr_guide)[GRID_SIZE][GRID_SIZE], char(&arr_vis)[GRID_SIZE][GRID_SIZE], int x, int y){
+
+	if(in_grid(x-1, y) && arr_guide[x-1][y] == '.' && arr_vis[x-1][y] != '.'){
+		arr_vis[x-1][y] = '.';
+		get_path(arr_guide,arr_vis,x-1,y);
+	} 
+	if(in_grid(x+1, y) && arr_guide[x+1][y] == '.'  && arr_vis[x+1][y] != '.'){
+		arr_vis[x+1][y] = '.';
+		get_path(arr_guide,arr_vis,x+1,y);
+	} 
+	if(in_grid(x, y-1) && arr_guide[x][y-1] == '.'  && arr_vis[x][y-1] != '.'){
+		arr_vis[x][y-1] = '.';
+		get_path(arr_guide,arr_vis,x,y-1);
+	} 
+	if(in_grid(x, y+1) && arr_guide[x][y+1] == '.' && arr_vis[x][y+1] != '.'){
+		arr_vis[x][y+1] = '.';
+		get_path(arr_guide,arr_vis,x,y+1);
+	}
 	
+}
+
+bool place_flag(char (&arr_vis)[GRID_SIZE][GRID_SIZE], char(&arr_guide)[GRID_SIZE][GRID_SIZE]){
+	int x,y;
+	int count = 0;
+	cout << "Which square would you like to reveal? (1-10) (x y)> ";
+	cin >> y >> x;
+	x-=1;
+	y-=1;
+	if(in_grid(x,y)){
+		arr_vis[x][y] = 'F';
+		for(int i = 0; i < GRID_SIZE; i++){
+			for(int j = 0; j < GRID_SIZE; j++){
+				if(arr_vis[i][j] == 'F' && arr_guide[i][j] == '@')
+					count++;
+			}
+		}
+		if (count == MINES){
+			system("CLS");
+			cout << "You Win!" << endl;
+			return true;
+		}
+		
+	}else{
+		cout << "That point does not exist!" << endl;
+	}
+	return false;
+}
+
+void remove_flag(char (&arr_vis)[GRID_SIZE][GRID_SIZE]){
+	int x,y;
+	cout << "Which square would you like to reveal? (1-10) (x y)> ";
+	cin >> y >> x;
+	x-=1;
+	y-=1;
+	if(in_grid(x,y) && arr_vis[x][y] == 'F'){
+		arr_vis[x][y] == '#';
+	}else{
+		cout << "That is not a flag!" << endl;
+	}
 }
 
